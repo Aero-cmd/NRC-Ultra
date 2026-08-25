64bd1b7ab5002f5370f7d1c194d5a71e9e6abcbd
AeroStudios <strixzz39@gmail.com>
2026-08-25 14:39:49 +0100

    feat: add NRC Ultra branding constants

---
+++ src/config/nrcUltraBranding.ts
@@ -0,0 +1,6 @@
+export const NRC_ULTRA_BRANDING = {
+  name: 'NRC Ultra',
+  shortName: 'NRC',
+  tagline: 'Performance-first Minecraft client',
+  website: 'https://github.com/Aero-cmd/NRC-Ultra',
+} as const;
